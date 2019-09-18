d0bb0fc9ca059e619aee8673abcc52aedc55d8be
DeMarcus Kirby <tkd024@gmail.com>
2019-09-18 08:47:32 -0600

    Add styling to App

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
+  <div>
+    <img src="https://wallpapermemory.com/uploads/654/gallery-background-hd-1920x1200-193325.jpg"  class="background"/>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <h1>Welcome to Final-Countdown</h1>
-      <p>Enter search criteria to display photos.</p>
-      <input placeholder="search" />
+      <h2>Enter search criteria to display photos.</h2>
+      <div class='search'>
+      <input placeholder="Search..." :value="searchValue" v-on:change="handleChange"/>
       <button v-on:click="handleSubmit">Submit</button>
+      </div>
     <PhotoContainer v-bind:photos='photos'/>
+  </div>
   </div>
 </template>
 
@@ -19,16 +24,21 @@ export default {
   },
   data() {
     return {
-      photos: []
+      photos: [],
+      searchValue: ""
     }
   },
   methods: {
-    handleSubmit(e) {
-      const searchText = e.target.previousSibling.value;
+    handleSubmit() {
+      const searchText = this.searchValue;
       const url = 'https://api.unsplash.com/search/photos/?query=' + searchText + '&client_id=c6eeeedc8c658881d7eba602b260a5faeaed1be2144c5827eaf628940b049428'
       fetch(url)
       .then(response => response.json())
       .then(data => this.photos = data.results)
+      this.searchValue = ''
+    },
+    handleChange(e) {
+      this.searchValue = e.target.value
     }
   }
 }
@@ -40,7 +50,40 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+}
+
+h1 {
+  color: white;
+  margin-top: 0px;
+}
+
+body {
+  margin: 0px;
+}
+
+input {
+  font-size: 2rem;
+  height: 50px;
+  width: 35%;
+}
+
+button {
+  font-size: 1rem;
+  height: 56px;
+  width: 80px;
+}
+
+.background {
+  height: 100%;
+  position: fixed;
+  filter: brightness(25%);
+  width: 100%;
+  z-index: -1;
+}
+
+.search {
+  display: flex;
+  justify-content: center;
 }
 </style>
