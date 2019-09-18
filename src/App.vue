bdb538561baccd871500060553649d0223a9cb09
DeMarcus Kirby <tkd024@gmail.com>
2019-09-17 23:41:18 -0600

    Save data into local state

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <p>Enter search criteria to display photos.</p>
       <input placeholder="search" />
       <button v-on:click="handleSubmit">Submit</button>
-    <PhotoContainer msg="Working"/>
+    <PhotoContainer v-bind:photos='photos'/>
   </div>
 </template>
 
@@ -23,12 +23,12 @@ export default {
     }
   },
   methods: {
-    handleSubmit: (e) => {
-      const searchText = e.target.previousSibling.value
+    handleSubmit(e) {
+      const searchText = e.target.previousSibling.value;
       const url = 'https://api.unsplash.com/search/photos/?query=' + searchText + '&client_id=c6eeeedc8c658881d7eba602b260a5faeaed1be2144c5827eaf628940b049428'
       fetch(url)
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => this.photos = data.results)
     }
   }
 }
